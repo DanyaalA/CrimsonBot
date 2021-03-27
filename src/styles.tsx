cc70ccb4835ab767801dda4f3c793cf9ee8ea79b
DanyaalA <Danny13653@gmail.com>
2021-03-27 19:01:50 +0000

    Styled.tsx

---
+++ src/styles.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+export const BasePageStyle = styled.div`
+  padding: 0px 50px;
+  height: 100%;
+  width: calc(100% - 85px);
+  margin-left: 85px;
+  margin-top: 85px;
+  color: white;
+  position: absolute;
+  box-sizing: border-box;
+`;
+
+export const ContainerStyle = styled.div`
+  margin-top: 15px;
+  padding: 20px 0px 20px 15px;
+  width: 100%;
+  display: flex;
+  border-radius: 10px;
+  background-color: #161c1f;
+  //141617
+`;
