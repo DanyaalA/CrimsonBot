241c80b0981615ae084f61995ee69b4949958749
DanyaalA <Danny13653@gmail.com>
2021-03-28 21:17:48 +0100

    Theme System

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { DarkTheme } from "./styles/Themes";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={DarkTheme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
