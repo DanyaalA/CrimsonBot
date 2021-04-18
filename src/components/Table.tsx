f4a74b6a22b70d6a8f299f8f0d3ad22d1b4a99e8
DanyaalA <Danny13653@gmail.com>
2021-04-18 15:35:51 +0100

    Converted Table into an actual table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,62 +8,35 @@ type LogProps = {
 
 export const Table = ({ logs }: LogProps) => {
   return (
-    <TableStyle>
-      <Header>
-        <p>User</p>
-        <p>Message</p>
-        <p>Subreddit</p>
-        <p>Post</p>
-        <p>Time</p>
-      </Header>
-
-      <Content>
-        {logs.map((log) => (
-          <TableItem>
+    <StyledTable>
+      <tr>
+        <th>User</th>
+        <th>Message</th>
+        <th>Subreddit</th>
+        <th>Post</th>
+        <th>Time</th>
+      </tr>
+      {logs.map((log) => (
+        <tr>
+          <td>
             <a href={`https://reddit.com/u/${log.username}/`}>{log.username}</a>
-            <p>{log.message}</p>
-            <p>{log.subreddit}</p>
+          </td>
+          <td>{log.message}</td>
+          <td>{log.subreddit}</td>
+          <td>
             <a
               href={`https://reddit.com/r/${log.subreddit}/comments/${log.subId}`}
             >
-              Link Here
+              Here
             </a>
-            <p>{log.time}</p>
-          </TableItem>
-        ))}
-      </Content>
-    </TableStyle>
+          </td>
+          <td>{log.time}</td>
+        </tr>
+      ))}
+    </StyledTable>
   );
 };
-
-const TableStyle = styled.div``;
-
-const Header = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding-bottom: 4px;
-  margin-bottom: 4px;
-  border-bottom: 1px solid rgba(61, 55, 95, 0.3);
-  justify-content: space-between;
-  padding-right: 130px;
-  margin-right: 30px;
-  p {
-    //margin-right: 20rem;
-    font-size: 23px;
-  }
-`;
-
-const Content = styled.div``;
-
-const TableItem = styled.div`
-  display: flex;
-  margin-bottom: 5px;
-  justify-content: space-between;
-  padding-right: 130px;
-  p {
-    //margin: 0 18rem 0 0;
-    //margin-right: 20%;
-  }
+const StyledTable = styled.table`
   a {
     text-decoration: none;
     color: #0e48e9;
