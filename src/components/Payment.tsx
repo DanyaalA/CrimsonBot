7513c93e34fa19586d6ce9a38b4716863ab6162d
DanA <Danny13653@gmail.com>
2021-08-08 13:47:01 +0100

    Fixed Equal Checker

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -19,17 +19,17 @@ export const Payment = ({ payments, payment, setPayments }: PaymentProps) => {
   };
 
   const updatePayments = (e: any, inputType: InputType) => {
-    if (inputType == InputType.Name) {
+    if (inputType === InputType.Name) {
       payment.name = e.target.value;
-    } else if (inputType == InputType.Value) {
+    } else if (inputType === InputType.Value) {
       payment.value = e.target.value;
-    } else if (inputType == InputType.Type) {
+    } else if (inputType === InputType.Type) {
       payment.type = e.target.value;
     }
 
     setPayments(
       payments.map((item: Payments) => {
-        if (item.id == payment.id) {
+        if (item.id === payment.id) {
           return payment;
         } else {
           return item;
